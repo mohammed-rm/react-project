fcb84016e4c1d7271ea9376f39311e1d0cf45486
mohammed_rm <mohammed.rmich@etu.univ-tours.fr>
2022-02-27 23:24:42 +0100

    pages routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-
+import {Route} from "react-router-dom";
+import {Routes} from "react-router";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetups";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
-  return <div> Hello </div>
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<AllMeetupsPage/>}/>
+                <Route path="/new-meetup" element={<NewMeetupPage/>}/>
+                <Route path="/favorites" element={<FavoritesPage/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
